b11fb2de7779aa8938192f14199f99579d616f33
magister <58708723+magister6239@users.noreply.github.com>
2025-10-31 02:21:57 +0300

    Add files via upload

--- proj4.cpp
+++ proj4.cpp
@@ -9,29 +9,29 @@ double max_err(int N, double a, double b, double y0) {
     double h = (b - a) / N;
     double x = a;
     double y = y0;
-    double maxe = std::fabs(y - y_exact(x));
+    double max = std::fabs(y - y_exact(x));
 
     for (int i = 0; i < N; ++i) {
         double ynext = y + h * (2.0 * x * y);
         x += h;
         y = ynext;
         double e = std::fabs(y - y_exact(x));
-        if (e > maxe) maxe = e;
+        if (e > max) max = e;
     }
-    return maxe;
+    return max;
 }
 
 int main() {
     const double a = 0, b = 1, y0 = 1;
     int N = 32;
-    double htable = (b - a) / N;
+    double h = (b - a) / N;
 
     std::cout << "j        x_j    y_pred     y_real    error\n";
     double x = a, y = y0;
     std::cout << 0 << " " << x << " " << y << " " << y_exact(x) << " " << std::fabs(y - y_exact(x)) << "\n";
     for (int j = 0; j < N; ++j) {
-        double ynext = y + htable * (2 * x * y);
-        x += htable;
+        double ynext = y + h * (2 * x * y);
+        x += h;
         y = ynext;
         std::cout << (j + 1) << "           " << x << "           " << y << "           " << y_exact(x) << "           " << std::fabs(y - y_exact(x)) << "\n";
     }
