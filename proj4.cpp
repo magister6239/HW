bd04f626fda22b384c74b6ab961afe94fa33acc2
magister <58708723+magister6239@users.noreply.github.com>
2025-10-31 02:20:00 +0300

    Add files via upload

---
+++ proj4.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cmath>
+
+double y_exact(double x) {
+    return std::exp(x * x);
+}
+
+double max_err(int N, double a, double b, double y0) {
+    double h = (b - a) / N;
+    double x = a;
+    double y = y0;
+    double maxe = std::fabs(y - y_exact(x));
+
+    for (int i = 0; i < N; ++i) {
+        double ynext = y + h * (2.0 * x * y);
+        x += h;
+        y = ynext;
+        double e = std::fabs(y - y_exact(x));
+        if (e > maxe) maxe = e;
+    }
+    return maxe;
+}
+
+int main() {
+    const double a = 0, b = 1, y0 = 1;
+    int N = 32;
+    double htable = (b - a) / N;
+
+    std::cout << "j        x_j    y_pred     y_real    error\n";
+    double x = a, y = y0;
+    std::cout << 0 << " " << x << " " << y << " " << y_exact(x) << " " << std::fabs(y - y_exact(x)) << "\n";
+    for (int j = 0; j < N; ++j) {
+        double ynext = y + htable * (2 * x * y);
+        x += htable;
+        y = ynext;
+        std::cout << (j + 1) << "           " << x << "           " << y << "           " << y_exact(x) << "           " << std::fabs(y - y_exact(x)) << "\n";
+    }
+
+    int found = -1;
+    for (int N = 1; N <= 65536; ++N) {
+        if (max_err(N, a, b, y0) < 1e-2) {
+            found = N;
+            break;
+        }
+    }
+
+    if (found > 0)
+        std::cout << found;
+    else
+        std::cout << "No dice";
+
+    return 0;
+}
